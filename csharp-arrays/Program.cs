b87429623f9e671dcde496a090467fe28da842dd
Muhammad Abbas <i-muhammadabbas@two.cx>
2024-09-24 20:56:10 +0500

    Understand: foreach loop, for loop, if, else if, else, boolean other operations in c# /understood.

---
+++ csharp-arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿/*string[] fradulentOrderIDs = new string[3];
+
+fradulentOrderIDs[0] = "A123";
+fradulentOrderIDs[1] = "B456";
+fradulentOrderIDs[2] = "C789";
+
+Console.WriteLine($"First: {fradulentOrderIDs[0]}");
+fradulentOrderIDs[0] = "D000";
+Console.WriteLine($"Reassign First: {fradulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fradulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fradulentOrderIDs[2]}"); */
+
+/*string[] fradulentOrderIDs = { "A123", "B456", "C789" };
+Console.WriteLine($"First: {fradulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fradulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fradulentOrderIDs[2]}");
+
+Console.WriteLine($"There are {fradulentOrderIDs.Length} fradulent orders to process."); */
+
+// string[] names = { "Abbas", "Awais", "Saad" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
